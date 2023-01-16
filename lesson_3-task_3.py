b1d18258bb5337f943070561996d67c36323474d
ShamalovaElena <96693713+ShamalovaElena@users.noreply.github.com>
2023-01-16 16:01:05 +0300

    add files

---
+++ lesson_3-task_3.py
@@ -0,0 +1,14 @@
+# Реализовать функцию my_func(),
+# которая принимает три позиционных аргумента и возвращает сумму наибольших двух аргументов.
+
+def my_func(a, b, c):
+    if a >= b and b >= c:
+        return a + b
+    elif a > b and a < c:
+        return a + c
+    else:
+        return b + c
+
+print(f'Result - {my_func(int(input("Введите первый аргумент ")), int(input("Введите второй аргумент ")), int(input("Введите третий аргумент ")))}')
+
+
